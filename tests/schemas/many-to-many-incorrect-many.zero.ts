189b9c9e55a9a396c4b144c64efbb23aaea7f10b
Chase Adams <c@cadams.io>
2025-01-20 20:30:35 -0700

    fix: zero

---
+++ tests/schemas/many-to-many-incorrect-many.zero.ts
@@ -0,0 +1,70 @@
+import {
+  ANYONE_CAN,
+  createSchema,
+  definePermissions,
+  type Schema,
+} from "@rocicorp/zero";
+import { createZeroSchema } from "../../src";
+import * as manyToMany from "./many-to-many.schema";
+
+export const schema = createSchema(
+  createZeroSchema(manyToMany, {
+    version: 1,
+    tables: {
+      user: {
+        id: true,
+        name: true,
+      },
+      group: {
+        id: true,
+        name: true,
+      },
+      users_to_group: {
+        user_id: true,
+        group_id: true,
+      },
+    },
+    manyToMany: {
+      user: {
+        groups: [
+          {
+            sourceField: "id",
+            destTable: "group",
+            destField: "id",
+          },
+          {
+            sourceField: "id",
+            destTable: "users_to_group",
+            // destField: "group_id", // missing field
+          } as any,
+        ],
+      },
+    },
+  }),
+);
+
+export const permissions = definePermissions<{}, Schema>(schema, () => {
+  return {
+    group: {
+      row: {
+        insert: ANYONE_CAN,
+        update: ANYONE_CAN,
+        delete: ANYONE_CAN,
+      },
+    },
+    user: {
+      row: {
+        insert: ANYONE_CAN,
+        update: ANYONE_CAN,
+        delete: ANYONE_CAN,
+      },
+    },
+    users_to_group: {
+      row: {
+        insert: ANYONE_CAN,
+        update: ANYONE_CAN,
+        delete: ANYONE_CAN,
+      },
+    },
+  };
+});
