57c3e955a36215257051dbd7c1a47b9e4f017c6f
Chase Adams <c@cadams.io>
2025-01-24 14:04:21 -0700

    fix: tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    environment: "jsdom",
+    globals: true,
     testTimeout: 15000,
     include: ["tests/**/*.test.ts"],
     typecheck: {
