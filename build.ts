8a3033f35a1d8693d7087ee13010cb5a08ad29de
Chase Adams <c@cadams.io>
2025-04-19 14:02:55 -0700

    fix: removed unrelated changes

--- build.ts
+++ build.ts
@@ -4,21 +4,27 @@ import pkg from "./package.json";
 
 const main = async () => {
   await tsup.build({
-    entry: ["src/index.ts"],
+    entryPoints: ["./src/index.ts"],
     outDir: "./dist",
     splitting: false,
     dts: true,
+    entry: ["src/index.ts"],
     format: ["cjs", "esm"],
   });
 
-  await tsup.build({
-    entry: ["src/cli.ts"],
-    outDir: "./dist",
-    splitting: false,
-    format: ["cjs"],
-    esbuildOptions: (opts) => {
-      opts.banner ??= {};
-      opts.banner.js = "#!/usr/bin/env node";
+  esbuild.buildSync({
+    entryPoints: ["./src/cli.ts"],
+    bundle: true,
+    outfile: "dist/bin.cjs",
+    format: "cjs",
+    target: "node16",
+    platform: "node",
+    external: ["esbuild", "tsx"],
+    define: {
+      "process.env.DRIZZLE_ZERO_VERSION": `"${pkg.version}"`,
+    },
+    banner: {
+      js: `#!/usr/bin/env node`,
     },
   });
 };
