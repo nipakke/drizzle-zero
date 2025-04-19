711973e81aa1056fffe44d2f06bf09cacd7f0b47
Max Stevens <maxstevens2708@gmail.com>
2025-04-19 21:24:17 +0200

    fix: include drizzle as peer dep

--- build.ts
+++ build.ts
@@ -4,27 +4,21 @@ import pkg from "./package.json";
 
 const main = async () => {
   await tsup.build({
-    entryPoints: ["./src/index.ts"],
+    entry: ["src/index.ts"],
     outDir: "./dist",
     splitting: false,
     dts: true,
-    entry: ["src/index.ts"],
     format: ["cjs", "esm"],
   });
 
-  esbuild.buildSync({
-    entryPoints: ["./src/cli.ts"],
-    bundle: true,
-    outfile: "dist/bin.cjs",
-    format: "cjs",
-    target: "node16",
-    platform: "node",
-    external: ["esbuild", "tsx"],
-    define: {
-      "process.env.DRIZZLE_ZERO_VERSION": `"${pkg.version}"`,
-    },
-    banner: {
-      js: `#!/usr/bin/env node`,
+  await tsup.build({
+    entry: ["src/cli.ts"],
+    outDir: "./dist",
+    splitting: false,
+    format: ["cjs"],
+    esbuildOptions: (opts) => {
+      opts.banner ??= {};
+      opts.banner.js = "#!/usr/bin/env node";
     },
   });
 };
